ec1aa9821855ab0b39ec0507831d26e1eb76428f
Hugues4049 <titoebene@gmail.com>
2025-05-01 22:54:26 +0200

    création des pages

--- mapage/urls.py
+++ mapage/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home")
+    path('', views.index, name='index'),  # <- ceci gère la racine "/"
+    path('profil/', views.profil, name='profil'),
+    path('parcours-academique/', views.parcours_academique, name='parcours_academique'),
+    path('experiences/', views.experiences, name='experiences'),
+    path('certifications/', views.certifications, name='certifications'),
+    path('projets/', views.projets, name='projets'),
+    path('competences/', views.competences, name='competences'),
+    path('contact/', views.contact, name='contact'),
 ]
--- mapage/views.py
+++ mapage/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
+def profil(request):
+    return render(request, 'profil.html')
+
+def parcours_academique(request):
+    return render(request, 'parcours-academique.html')
+
+def experiences(request):
+    return render(request, 'experiences.html')
+
+def certifications(request):
+    return render(request, 'certifications.html')
+
+def projets(request):
+    return render(request, 'projets.html')
+
+def competences(request):
+    return render(request, 'competences.html')
+
+def contact(request):
+    return render(request, 'contact.html')
